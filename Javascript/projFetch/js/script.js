aae30a5f00885e6a9a079d185e5ee4433386b8b9
MateusJSouza <mateusjdev@gmail.com>
2022-07-06 14:13:40 -0300

    feat: exercício de notas escolares

---
+++ Javascript/projFetch/js/script.js
@@ -0,0 +1,5 @@
+const url = "https://jsonplaceholder.typicode.com/posts" // API
+
+// Selecionando elementos
+const loadingElement = document.querySelector('#loading');
+const postsContainer = document.querySelector('#posts-container');
