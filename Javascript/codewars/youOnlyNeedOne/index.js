b035034128e650b498a87c601a39ff9b2c1ce88b
MateusJSouza <mateusjdev@gmail.com>
2022-07-25 13:05:16 -0300

    feat: you only need one

---
+++ Javascript/codewars/youOnlyNeedOne/index.js
@@ -0,0 +1,7 @@
+function check(a, x) {
+  // x = 66;
+  // a = [66, 107]
+  return a.includes(x);
+}
+
+// includes() -> determina se um conjunto de caracteres pode ser encontrado dentro de outra string, retornando true ou false.
