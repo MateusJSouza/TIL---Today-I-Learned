cb7594a1b55bdc4b76c84fbc0eb1b1d49bcd4de5
MateusJSouza <mateusjdev@gmail.com>
2022-07-29 16:29:09 -0300

    feat: The highest profit wins

---
+++ Javascript/codewars/theHighestProfitWins/index.js
@@ -0,0 +1,9 @@
+function minMax(arr) {
+  return [Math.min(...arr), Math.max(...arr)];
+}
+
+/* 
+  ...arr -> spread operator, pega todos os números dentro que há dentro do array
+  Math.min() o menor número dentro do array
+  Math.max() - retorna o maior número dentro do array
+*/
