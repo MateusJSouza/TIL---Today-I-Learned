41a491a02a19a284794d3a4b248432da373b3464
MateusJSouza <mateusjdev@gmail.com>
2022-07-29 16:38:24 -0300

    Beginner Series #2 Clock

---
+++ Javascript/codewars/BeginnerSeries#2Clock/index.js
@@ -0,0 +1,15 @@
+function past(h, m, s){
+  return h * 3600000 + m * 60000 + s * 1000;
+}
+
+/*
+  - Multiplicando as horas por 1 hora em milissegundos
+  - Minutos por 1 minuto em milissegundos
+  - Segundos por 1 segundo em milissegundos
+
+  E no fim é mostrado o resultado das somas
+
+  1 hora em milissegundos = 3600000
+  1 minuto em milissegundos = 60000
+  1 segundo em milissegundos = 1000
+*/
