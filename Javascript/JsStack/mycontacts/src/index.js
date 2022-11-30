e906d3caccd09d1c0a452d6fe5279f246303f6a6
Mateus Jesus <mateusjdev@gmail.com>
2022-11-30 19:56:17 -0300

    feat: add ESLint e editorconfig

--- Javascript/JsStack/mycontacts/src/index.js
+++ Javascript/JsStack/mycontacts/src/index.js
@@ -3,14 +3,14 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-  response.send('Hello World!')
-})
+  response.send('Hello World!');
+});
 
 app.listen(3001, () => {
   console.log('🔥 Server started at http://localhost:3001');
-})
+});
 
 /*
   Para utilizarmos o nodemon, basta digitar no terminal o seguinte comando:
-  npx nodemon src/index.js (ou seja, o caminho da aplicação)  
+  npx nodemon src/index.js (ou seja, o caminho da aplicação)
 */
