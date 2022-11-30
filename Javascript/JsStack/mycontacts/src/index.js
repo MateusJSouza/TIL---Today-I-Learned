4e9d98fe61ee966e2a36491a3321944cf564a414
Mateus Jesus <mateusjdev@gmail.com>
2022-11-30 19:46:58 -0300

    feat: instalando e usando o nodemon no servidor

--- Javascript/JsStack/mycontacts/src/index.js
+++ Javascript/JsStack/mycontacts/src/index.js
@@ -9,3 +9,8 @@ app.get('/', (request, response) => {
 app.listen(3001, () => {
   console.log('🔥 Server started at http://localhost:3001');
 })
+
+/*
+  Para utilizarmos o nodemon, basta digitar no terminal o seguinte comando:
+  npx nodemon src/index.js (ou seja, o caminho da aplicação)  
+*/
