d11267b9cc2f1b21bcb2da4b1294a69bb533e703
Mateus Jesus <mateusjdev@gmail.com>
2022-12-01 13:18:14 -0300

    feat: map, filter e reduce

--- Javascript/JsStack/09-hof.js
+++ Javascript/JsStack/09-hof.js
@@ -32,5 +32,36 @@ const some = array.some((product) => product.price < 1000);
 
 // .every
 const every = array.every((product) => product.price > 1000);
-console.log({ every })
+// console.log({ every })
 // true, pois todos os preços do meu carrinho são maior que mil
+
+// map
+const map = array.map((product) => ({
+  ...product,
+  subtotal: product.quantity * product.price
+}));
+
+// console.log({ map })
+/*
+	Resultado:
+	[
+    { name: 'iPhone', price: 5000, quantity: 2, subtotal: 10000 },
+    { name: 'MacBook Pro', price: 20000, quantity: 1, subtotal: 20000 },
+    { name: 'Magic Mouse', price: 1001, quantity: 5, subtotal: 5005 }
+  ]
+*/
+
+// filter
+const filter = array.filter((product) => product.quantity >= 2);
+// console.log({ filter });
+
+// reduce
+const reduce = array.reduce((accumulator, product) => {
+  return accumulator + (product.price * product.quantity);
+}, 0)
+
+// console.log({ reduce })
+/*
+	Resultado:
+	{ reduce: 35005 }
+*/
