453278c6cfc502a2264ecd36e1b5f92753a01de6
Mateus Jesus <mateusjdev@gmail.com>
2022-12-12 17:38:22 -0300

    feat: tipando as propriedades com o PropTypes

--- Javascript/JsStack/react-fundamentos/src/App.js
+++ Javascript/JsStack/react-fundamentos/src/App.js
@@ -15,9 +15,26 @@ export function App() {
 
       {/* Passando as propriedades no formato de objeto */}
       <Post
+        likes={20}
         post={{
           title: 'Título da notícia 01',
           subtitle: 'Subtítulo da notícia 01'
+        }}
+      />
+
+      <Post
+        likes={10}
+        post={{
+          title: 'Título da notícia 02',
+          subtitle: 'Subtítulo da notícia 02'
+        }}
+      />
+
+      <Post
+        likes={50}
+        post={{
+          title: 'Título da notícia 03',
+          subtitle: 'Subtítulo da notícia 03'
         }}
       />
     </>
--- Javascript/JsStack/react-fundamentos/src/components/Post.js
+++ Javascript/JsStack/react-fundamentos/src/components/Post.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
 export function Post(props) {
   return (
@@ -8,6 +9,17 @@ export function Post(props) {
         <small>{props.post.subtitle}</small>
       </article>
       <br />
+      Média: {props.likes / 2}
     </>
   )
 }
+
+// Tipando as propriedades com o PropTypes
+Post.propTypes = {
+  likes: PropTypes.number.isRequired,
+  // Tipando um objeto
+  post: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+  }).isRequired,
+};
