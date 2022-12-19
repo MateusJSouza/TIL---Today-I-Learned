5b4d76607995c765c12594d8528b1534fb3278e6
Mateus Jesus <mateusjdev@gmail.com>
2022-12-19 17:32:56 -0300

    feat: adicionando styled-components ao projeto

---
+++ Javascript/JsStack/react-fundamentos/src/components/App/index.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+
+import { Header } from '../Header/index';
+import { Post } from '../Post/index';
+
+import { ThemeProvider } from '../../context/ThemeContext';
+
+import styles from './App.scss';
+
+
+// Props -> propriedades
+export function App() {
+
+  const [posts, setPosts] = useState([
+    { id: Math.random(), title: 'Title#01', subtitle: 'Sub#01', likes: 20, read: true, removed: false },
+    { id: Math.random(), title: 'Title#02', subtitle: 'Sub#02', likes: 10, read: false, removed: false },
+    { id: Math.random(), title: 'Title#03', subtitle: 'Sub#03', likes: 50, read: true, removed: false },
+    { id: Math.random(), title: 'Title#04', subtitle: 'Sub#04', likes: 80, read: false, removed: false },
+  ]);
+
+  function handleRefresh() {
+    // Mantendo os posts e adicionando os novos em cada atualização
+    setPosts((prevState) => [
+      ...prevState,
+      {
+        id: Math.random(),
+        title: `Title#0${prevState.length + 1}`,
+        subtitle: `Sub#0${prevState.length + 1}`,
+        likes: 50
+      }
+    ])
+  }
+
+  function handleRemovePost(postId) {
+    setPosts((prevState) => prevState.map(
+      post => (
+        post.id === postId
+          ? { ...post, removed: true }
+          : post
+      )
+    ));
+  }
+
+  return (
+    <ThemeProvider>
+      <Header>
+        <h2 className={styles.title}>
+          Posts da semana
+          <button onClick={handleRefresh}>Atualizar</button>
+          <button onClick={() => {setPosts([])}}>Limpar</button>
+        </h2>
+      </Header>
+      <hr />
+
+      {posts.map(post => (
+        <Post
+          key={post.id}
+          onRemove={handleRemovePost}
+          post={post}
+        />
+      ))}
+    </ThemeProvider>
+  )
+}
---
+++ Javascript/JsStack/react-fundamentos/src/components/Post/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+import { PostHeader } from './PostHeader/PostHeader';
+
+import styles from './Post.scss';
+
+export function Post(props) {
+  return (
+    <article
+      className={
+        props.post.removed
+          ? styles.postDeleted
+          : styles.post
+      }
+    >
+      <PostHeader
+        onRemove={props.onRemove}
+        post={{
+          id: props.post.id,
+          title: props.post.title,
+          read: props.post.read,
+        }}
+      />
+      <br />
+      <small>{props.post.subtitle}</small>
+      <br />
+      Média: {props.post.likes / 2}
+    </article>
+  );
+}
+
+// Tipando as propriedades com o PropTypes
+Post.propTypes = {
+  onRemove: PropTypes.func.isRequired,
+  // Tipando um objeto
+  post: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    likes: PropTypes.number.isRequired,
+    read: PropTypes.bool.isRequired,
+    removed: PropTypes.bool.isRequired,
+  }).isRequired,
+};
