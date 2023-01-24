82db54aa57c88b6a7f32eeec150322a558584c7d
Mateus Jesus <mateusjdev@gmail.com>
2023-01-24 15:56:54 -0300

    feat: criando método response.send

--- firstapi/src/controllers/UserController.js
+++ firstapi/src/controllers/UserController.js
@@ -12,8 +12,7 @@ module.exports = {
       return a.id > b.id ? 1 : -1;
     });
 
-    response.writeHead(200, { 'Content-Type': 'application/json' });
-    response.end(JSON.stringify(sortedUsers)); // convertendo o array em string com o JSON.stringfy
+    response.send(200, sortedUsers)
   },
 
   getUserById(request, response) {
@@ -22,11 +21,9 @@ module.exports = {
     const user = users.find(user => user.id === Number(id));
 
     if (!user) {
-      response.writeHead(400, { 'Content-Type': 'application/json' });
-      response.end(JSON.stringify({ error: 'User not found' }))
+      return response.send(400, { error: 'User not found' });
     }
 
-    response.writeHead(200, { 'Content-Type': 'application/json' });
-    response.end(JSON.stringify(user));
+    response.send(200, user);
   }
 };
--- firstapi/src/index.js
+++ firstapi/src/index.js
@@ -29,6 +29,11 @@ const server = http.createServer((request, response) => {
     request.query = Object.fromEntries(parsedUrl.searchParams)
     request.params = { id };
 
+    response.send = (statusCode, body) => {
+      response.writeHead(statusCode, { 'Content-Type': 'application/json' });
+      response.end(JSON.stringify(body));
+    }
+
     route.handler(request, response)
   } else {
     response.writeHead(404, { 'Content-Type': 'text/html' });
