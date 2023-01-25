7d35c7499356e158cf63a954c06115ec1db603b9
Mateus Jesus <mateusjdev@gmail.com>
2023-01-25 19:12:30 -0300

    feat: configurando eslint e editorConfig

--- Javascript/JsStack/mycontacts/.eslintrc.js
+++ Javascript/JsStack/mycontacts/.eslintrc.js
@@ -1,17 +1,15 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es2021: true,
-    node: true,
-  },
-  extends: 'airbnb-base',
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-  },
-  rules: {
-    'class-methods-use-this': 'off',
-  },
-};
+    "env": {
+        "commonjs": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": "airbnb-base",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+    }
+}
