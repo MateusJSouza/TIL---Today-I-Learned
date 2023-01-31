989ff706bab9125913265984713dd34d26c9f9a4
Mateus Jesus <mateusjdev@gmail.com>
2023-01-31 17:25:48 -0300

    fix: add comentários

--- Javascript/hackerrank/birthdayCakeCandles/index.js
+++ Javascript/hackerrank/birthdayCakeCandles/index.js
@@ -2,17 +2,29 @@ function birthdayCakeCandles(arr) {
   let max = 0;
   let candles = 0;
 
+  /* 
+    Percorrendo o arr, se o valor de "max" for menor que o elemento atual
+    "max" é atualizado para esse elemento.
+  */
   for (const element of arr) {
     if (max < element) {
       max = element;
     }
   }
 
+  /* 
+    Também percorre o array, e se o elemtno atual for igual ao valor de "max",
+    a variável "candles" é incrementada em 1.
+  */
   for (const element of arr) {
     if (max == element) {
       candles++;
     }
   }
 
+  /*
+    Finalmente a função retorna o valor de "candles", que representa a quantidade
+    que o número mais alto aparece no array "arr"
+  */
   return candles;
 }
