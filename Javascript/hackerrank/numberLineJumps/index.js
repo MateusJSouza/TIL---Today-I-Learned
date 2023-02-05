f9d0adb2c5c1a5099bb014f87b44fc017819d08f
Mateus Jesus <mateusjdev@gmail.com>
2023-02-04 21:52:15 -0300

    fix: alterando lógica da função

--- Javascript/hackerrank/numberLineJumps/index.js
+++ Javascript/hackerrank/numberLineJumps/index.js
@@ -7,13 +7,19 @@
 */
 
 function kangaroo(x1, x2, v1, v2) {
-  if (v1 <= v2) {
-    return "NO";
-  } else {
-    // cálculo do número de saltos que cada canguru precisa dar para se encontrar com o outro, caso esse número for positivo e inteiro, retorna "YES", caso contrário, retorna "NO"
-    let meet = (x2 - x1) / (v1 - v2);
-    if (meet >= 0 && Number.isInteger(meet)) {
-      return "YES";
-    } else return "NO";
+  let jump1 = x1;
+  let jump2 = x2;
+  let yesNo = 'NO';
+
+  for (let i = 0; i <= 10000; i++) {
+    jump1 += v1;
+    jump2 += v2;
+
+    if (jump1 === jump2) {
+      yesNo = 'YES';
+    }
   }
+  return yesNo;
 }
+
+console.log(kangaroo(0, 3, 4, 2))
