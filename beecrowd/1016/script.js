ea7ab82db58775f8ba6ffc7f1840d23c186c73d6
Mateus Jesus <mateusjdev@gmail.com>
2023-02-15 19:10:30 -0300

    feat: exercício 1016 outra forma de fazer

--- beecrowd/1016/script.js
+++ beecrowd/1016/script.js
@@ -1,6 +1,9 @@
 const input = require('fs').readFileSync("stdin", "utf-8");
 
-// Regra de 3
-const qtdMinutos = (60 * input) / 30;
+// 1ª forma de fazer com regra de 3
+// const qtdMinutos = (60 * input) / 30;
 
-console.log(qtdMinutos + ' minutos');
+// console.log(qtdMinutos + ' minutos');
+
+// 2ª forma -> multiplicando por 2 o valor de input
+console.log((input * 2) + ' minutos');
