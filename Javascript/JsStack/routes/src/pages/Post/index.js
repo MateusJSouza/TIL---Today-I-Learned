12888cb4df348b4b11c3026fa6f09c868f07fe63
Mateus Jesus <mateusjdev@gmail.com>
2023-03-16 20:40:49 -0300

    parâmetros e query params em class components

--- Javascript/JsStack/routes/src/pages/Post/index.js
+++ Javascript/JsStack/routes/src/pages/Post/index.js
@@ -1,15 +1,29 @@
-import React, { useMemo } from "react";
-import { useParams, useLocation } from 'react-router-dom';
+import React from "react";
 
-export function Post() {
-  const params = useParams();
-  const { search } = useLocation();
+export class Post extends React.Component {
+  constructor(props) {
+    super(props);
 
-  // Só criará uma nova instância caso o valor do search mudar
-  const queryParams = useMemo(() => new URLSearchParams(search), [search]);
+    const { search } = this.props.location;
+    this.queryParams = new URLSearchParams(search);
+  }
 
-  console.log(queryParams.get('meuQueryParam'));
-  console.log(params);
+  render() {
+    console.log(this.queryParams.get('meuQueryParam'));
 
-  return <h1>Post page</h1>
+    return <h1>Post page</h1>
+  }
 }
+
+// export function Post() {
+//   const params = useParams();
+//   const { search } = useLocation();
+
+//   // Só criará uma nova instância caso o valor do search mudar
+//   const queryParams = useMemo(() => new URLSearchParams(search), [search]);
+
+//   console.log(queryParams.get('meuQueryParam'));
+//   console.log(params);
+
+//   return <h1>Post page</h1>
+// }
