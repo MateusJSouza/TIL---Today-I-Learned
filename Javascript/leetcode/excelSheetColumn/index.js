ba02b7d62263f5da866a149bd5b068ab03504909
MateusJSouza <mateusjdev@gmail.com>
2023-05-11 22:36:17 -0300

    excel sheet column

---
+++ Javascript/leetcode/excelSheetColumn/index.js
@@ -0,0 +1,11 @@
+var convertToTitle = function(columnNumber) {
+  let result = '';
+
+  while (columnNumber > 0) {
+    const res = (columnNumber - 1) % 26;
+    result = String.fromCharCode(65 + res) + result;
+    columnNumber = Math.floor((columnNumber - 1) / 26);
+  }
+
+  return result;
+};
