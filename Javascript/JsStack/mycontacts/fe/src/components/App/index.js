6658e0e6fea9419d1c06e38a4d1fbc5723b1c5e2
MateusJSouza <mateusjdev@gmail.com>
2023-05-26 16:59:31 -0300

    feat: adding contactsList component

--- Javascript/JsStack/mycontacts/fe/src/components/App/index.js
+++ Javascript/JsStack/mycontacts/fe/src/components/App/index.js
@@ -4,6 +4,7 @@ import GlobalStyles from '../../assets/styles/global';
 import defaultTheme from '../../assets/styles/themes/default';
 import { Container } from './styles';
 import { Header } from '../Header';
+import { ContactsList } from '../ContactsList';
 
 export function App() {
   return (
@@ -11,6 +12,7 @@ export function App() {
       <GlobalStyles />
       <Container>
         <Header />
+        <ContactsList />
       </Container>
     </ThemeProvider>
   );
---
+++ Javascript/JsStack/mycontacts/fe/src/components/ContactsList/styles.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin-top: 32px;
+
+
+`;
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  strong {
+    font-size: 24px;
+    color: #222;
+  }
+
+  a {
+    font-weight: bold;
+    color: ${({ theme }) => theme.colors.primary.main};
+    text-decoration: none;
+
+    border: 2px solid ${({ theme }) => theme.colors.primary.main};
+    border-radius: 4px;
+    padding: 8px 16px;
+    transition: all 0.2s ease-in;
+
+    &:hover {
+      background: ${({ theme }) => theme.colors.primary.main};
+      color: #fff;
+    }
+  }
+`;
