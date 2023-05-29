e04aa6da7664ea05008cdab317ad6b1265d38106
MateusJSouza <mateusjdev@gmail.com>
2023-05-29 18:45:55 -0300

    feat: home page creation and header refactoring

--- Javascript/JsStack/mycontacts/fe/src/pages/Home/index.js
+++ Javascript/JsStack/mycontacts/fe/src/pages/Home/index.js
@@ -1,5 +1,99 @@
+import {
+  Card,
+  Container, Header, InputSearchContainer, ListContainer,
+} from './styles';
+
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
+
 export function Home() {
   return (
-    <h1>Homepage</h1>
+    <Container>
+      <InputSearchContainer>
+        <input type="text" placeholder="Pesquise pelo nome..." />
+      </InputSearchContainer>
+
+      <Header>
+        <strong>3 contatos</strong>
+
+        <a href="/">Novo contato</a>
+      </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="Ícone de flecha" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Mateus Jesus</strong>
+              <small>instagram</small>
+            </div>
+
+            <span>mateus@devacademy.com.br</span>
+            <span>(88) 98885-6584</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Ícone de edição" />
+            </a>
+
+            <button type="button">
+              <img src={trash} alt="Ícone de lixeira" />
+            </button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Mateus Jesus</strong>
+              <small>instagram</small>
+            </div>
+
+            <span>mateus@devacademy.com.br</span>
+            <span>(88) 98885-6584</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Ícone de edição" />
+            </a>
+
+            <button type="button">
+              <img src={trash} alt="Ícone de lixeira" />
+            </button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Mateus Jesus</strong>
+              <small>instagram</small>
+            </div>
+
+            <span>mateus@devacademy.com.br</span>
+            <span>(88) 98885-6584</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Ícone de edição" />
+            </a>
+
+            <button type="button">
+              <img src={trash} alt="Ícone de lixeira" />
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
+    </Container>
   );
 }
