0002900ec2afe99398140aeac775059fb02cea30
MateusJSouza <mateusjdev@gmail.com>
2023-05-29 19:09:00 -0300

    feat: creating input and select component

--- Javascript/JsStack/mycontacts/fe/src/pages/NewContact/index.js
+++ Javascript/JsStack/mycontacts/fe/src/pages/NewContact/index.js
@@ -1,7 +1,21 @@
+import Input from '../../components/Input';
 import { PageHeader } from '../../components/PageHeader';
+import Select from '../../components/Select';
 
 export function NewContact() {
   return (
-    <PageHeader title="Novo contato" />
+    <>
+      <PageHeader
+        title="Novo contato"
+      />
+
+      <Input type="text" placeholder="Nome" />
+      <Select>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+      </Select>
+    </>
   );
 }
