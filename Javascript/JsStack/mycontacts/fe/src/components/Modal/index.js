e9f9ac0f3fc0f3c038ada0c76af7484df9d0dee1
MateusJSouza <mateusjdev@gmail.com>
2023-05-30 11:46:13 -0300

    feat: creating the modal component

---
+++ Javascript/JsStack/mycontacts/fe/src/components/Modal/index.js
@@ -0,0 +1,24 @@
+import { Overlay, Container, Footer } from './styles';
+
+import Button from '../Button';
+
+export function Modal() {
+  return (
+    <Overlay>
+      <Container>
+        <h1>
+          Título do modal
+        </h1>
+
+        <p>Corpo do modal</p>
+
+        <Footer>
+          <button type="button" className="cancel-button">Cancelar</button>
+          <Button type="button">
+            Deletar
+          </Button>
+        </Footer>
+      </Container>
+    </Overlay>
+  );
+}
