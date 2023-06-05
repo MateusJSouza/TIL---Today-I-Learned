32571b56e1ef299d0ef94fc7b5fb6d3852c3955e
MateusJSouza <mateusjdev@gmail.com>
2023-06-05 14:48:02 -0300

    feat: read calendar numbers

---
+++ Javascript/exercicios-js/entendendoAlgoritmos/lerNumerosDaAgenda.js
@@ -0,0 +1,8 @@
+function readCalendarNumbers(phonebook) {
+  for (let i = 0; i < phonebook.length; i++) {
+    const contact = phonebook[i];
+    const number = contact.number
+
+    console.log(number);
+  }
+}
