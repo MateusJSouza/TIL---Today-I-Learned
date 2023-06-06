9eb5232e9aec29079d71dac0fe3cc34053addba9
MateusJSouza <mateusjdev@gmail.com>
2023-06-06 13:25:42 -0300

    feat: validando os dados do input name e email

--- Javascript/JsStack/mycontacts/fe/src/components/ContactForm/index.js
+++ Javascript/JsStack/mycontacts/fe/src/components/ContactForm/index.js
@@ -1,9 +1,11 @@
 import PropTypes from 'prop-types';
-
 import { useState } from 'react';
-import { FormGroup } from '../FormGroup';
+
+import { isEmailValid } from '../../utils/isEmailValid';
 
 import { ButtonContainer, Form } from './styles';
+
+import { FormGroup } from '../FormGroup';
 import Input from '../Input';
 import Select from '../Select';
 import Button from '../Button';
@@ -13,8 +15,47 @@ export function ContactForm({ buttonLabel }) {
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
   const [category, setCategory] = useState('');
+  const [errors, setErrors] = useState([]);
 
-  const handleSubmit = (event) => {
+  console.log(errors);
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+
+    if (!event.target.value) {
+      setErrors((prevState) => [
+        ...prevState,
+        { field: 'name', message: 'Este campo é obrigatório' },
+      ]);
+    } else {
+      setErrors((prevState) => prevState.filter(
+        (error) => error.field !== 'name',
+      ));
+    }
+  }
+
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+
+    if (event.target.value && !isEmailValid(event.target.value)) {
+      const errorAlreadyExists = errors.find((error) => error.field === 'email');
+
+      if (errorAlreadyExists) {
+        return;
+      }
+
+      setErrors((prevState) => [
+        ...prevState,
+        { field: 'email', message: 'E-mail inválido.' },
+      ]);
+    } else {
+      setErrors((prevState) => prevState.filter(
+        (error) => error.field !== 'email',
+      ));
+    }
+  }
+
+  function handleSubmit(event) {
     event.preventDefault();
 
     console.log({
@@ -23,7 +64,7 @@ export function ContactForm({ buttonLabel }) {
       phone,
       category,
     });
-  };
+  }
 
   return (
     <Form onSubmit={handleSubmit}>
@@ -31,7 +72,7 @@ export function ContactForm({ buttonLabel }) {
         <Input
           placeholder="Nome"
           value={name}
-          onChange={(event) => setName(event.target.value)}
+          onChange={handleNameChange}
         />
       </FormGroup>
 
@@ -39,7 +80,7 @@ export function ContactForm({ buttonLabel }) {
         <Input
           placeholder="E-mail"
           value={email}
-          onChange={(event) => setEmail(event.target.value)}
+          onChange={handleEmailChange}
         />
       </FormGroup>
 
