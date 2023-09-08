471b1fc1f1f01d2ac3e9da37074919dc58e154d2
MateusJSouza <mateusjdev@gmail.com>
2023-09-08 15:39:50 -0300

    ✨ arbitrary values in tailwind

--- tailwind-next/tailwind.config.ts
+++ tailwind-next/tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      maxWidth: {
+        app: '700px'
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
