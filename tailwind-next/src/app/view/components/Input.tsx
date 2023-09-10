a1364091b3026f7ab68a4047023d1b5c5b9087e1
MateusJSouza <mateusjdev@gmail.com>
2023-09-10 17:29:40 -0300

    refactor: :recycle: composition pattern
    Division of a larger component into smaller components in order to make it easier to customize each part of the larger component.

---
+++ tailwind-next/src/app/view/components/Input.tsx
@@ -0,0 +1,33 @@
+import { ComponentProps } from 'react'
+
+export interface InputProps {
+  className: string
+}
+
+type InputPrefixProps = ComponentProps<'div'>
+
+export function InputPrefix(props: InputPrefixProps) {
+  return <div {...props} />
+}
+
+type InputControlProps = ComponentProps<'input'>
+
+export function InputControl(props: InputControlProps) {
+  return (
+    <input
+      className="flex-1 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 outline-none"
+      {...props}
+    />
+  )
+}
+
+export type InputRootProps = ComponentProps<'div'>
+
+export function InputRoot(props: InputRootProps) {
+  return (
+    <div
+      className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"
+      {...props}
+    />
+  )
+}
--- tailwind-next/src/app/view/components/Sidebar/index.tsx
+++ tailwind-next/src/app/view/components/Sidebar/index.tsx
@@ -4,20 +4,19 @@ import { MainNavigation } from './MainNavigation'
 import { NavItem } from './MainNavigation/NavItem'
 import { UsedSpaceWidget } from './UsedSpaceWidget'
 import { Profile } from './Profile'
+import { InputControl, InputPrefix, InputRoot } from '../Input'
 
 export function Sidebar() {
   return (
     <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
 
-      <div className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
-        <Search className="h-5 w-5 text-zinc-500" />
-        <input
-          type="text"
-          placeholder="Search"
-          className="flex-1 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 outline-none"
-        />
-      </div>
+      <InputRoot>
+        <InputPrefix>
+          <Search className="h-5 w-5 text-zinc-500" />
+        </InputPrefix>
+        <InputControl placeholder="Search" />
+      </InputRoot>
 
       <MainNavigation />
 
