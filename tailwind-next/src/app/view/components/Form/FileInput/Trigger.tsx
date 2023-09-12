5c43d8d138fb9d2b4d2d1511d2dee5c2e415ae98
MateusJSouza <mateusjdev@gmail.com>
2023-09-12 12:37:04 -0300

    refactor: :recycle: assigning a unique, random id to the label of the Trigger component

--- tailwind-next/src/app/view/components/Form/FileInput/Trigger.tsx
+++ tailwind-next/src/app/view/components/Form/FileInput/Trigger.tsx
@@ -1,12 +1,17 @@
+'use client'
+
 import { UploadCloud } from 'lucide-react'
+import { useFileInput } from './Root'
 
 export function Trigger() {
+  const { id } = useFileInput()
+
   return (
     <label
-      htmlFor="photo"
-      className="hover:bg-violet-25 group flex flex-1 cursor-pointer flex-col items-center gap-3 rounded-lg border border-zinc-300 px-6 py-4 text-center text-zinc-500 shadow-sm hover:border-violet-200"
+      htmlFor={id}
+      className="group flex flex-1 cursor-pointer flex-col items-center gap-3 rounded-lg border border-zinc-300 px-6 py-4 text-center text-zinc-500 shadow-sm hover:border-violet-200 hover:bg-violet-25"
     >
-      <div className="border-6 rounded-full border-zinc-50 bg-zinc-100 p-2 group-hover:border-violet-50 group-hover:bg-violet-100">
+      <div className="rounded-full border-6 border-zinc-50 bg-zinc-100 p-2 group-hover:border-violet-50 group-hover:bg-violet-100">
         <UploadCloud className="h-5 w-5 text-zinc-600 group-hover:text-violet-700" />
       </div>
 
@@ -15,7 +20,7 @@ export function Trigger() {
           <span className="font-semibold text-violet-700">Click to upload</span>{' '}
           or drag and drop
         </span>
-        <span className="text-xs">SVG, PNG, JPG or GIF</span>
+        <span className="text-xs">SVG, PNG, JPG or GIF (max. 800x400px)</span>
       </div>
     </label>
   )
