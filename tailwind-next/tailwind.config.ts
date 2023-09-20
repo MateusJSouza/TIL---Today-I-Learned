7c7c2949bbcfce46c4161d50ae600b6329ee3ce2
MateusJSouza <mateusjdev@gmail.com>
2023-09-20 19:57:30 -0300

    feat: :lipstick: animating opening of selects

--- tailwind-next/tailwind.config.ts
+++ tailwind-next/tailwind.config.ts
@@ -1,11 +1,7 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./src/**/*.tsx'],
   theme: {
     extend: {
       gridTemplateColumns: {
@@ -24,10 +20,30 @@ const config: Config = {
         },
       },
 
+      maxWidth: {
+        app: '700px',
+      },
+
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+
+      keyframes: {
+        slideDownAndFade: {
+          from: { opacity: '0', transform: 'translateY(-2px)' },
+          to: { opacity: '1', transform: 'translateY(0)' },
+        },
+        // slideUpAndFade: {
+        //   from: { opacity: '1' },
+        //   to: { opacity: '0' },
+        // },
+      },
+
+      animation: {
+        slideDownAndFade: 'slideDownAndFade 0.4s cubic-bezier(0.16, 1, 0.3, 1)',
+        slideUpAndFade: 'slideUpAndFade .0.4s cubic-bezier(0.16, 1, 0.3, 1)',
       },
     },
   },
