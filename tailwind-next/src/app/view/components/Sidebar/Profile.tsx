f9f84e365cde5169d6fa1c1770285eba2b05ae1f
MateusJSouza <mateusjdev@gmail.com>
2023-09-20 11:31:11 -0300

    fix: :adhesive_bandage: prettier fix

--- tailwind-next/src/app/view/components/Sidebar/Profile.tsx
+++ tailwind-next/src/app/view/components/Sidebar/Profile.tsx
@@ -2,7 +2,7 @@ import { LogOut } from 'lucide-react'
 
 export function Profile() {
   return (
-    <div className="grid-cols-profile grid gap-3">
+    <div className="grid grid-cols-profile gap-3">
       <img
         src="https://github.com/MateusJSouza.png"
         className="h-10 w-10 rounded-full"
