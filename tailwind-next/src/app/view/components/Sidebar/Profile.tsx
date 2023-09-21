c2b64c8dbb8ac2fc8cc43150bed0d2f10142fa55
MateusJSouza <mateusjdev@gmail.com>
2023-09-21 15:47:52 -0300

    feat: :lipstick: add ghost variant on buttons

--- tailwind-next/src/app/view/components/Sidebar/Profile.tsx
+++ tailwind-next/src/app/view/components/Sidebar/Profile.tsx
@@ -1,4 +1,5 @@
 import { LogOut } from 'lucide-react'
+import { Button } from '../Button'
 
 export function Profile() {
   return (
@@ -18,12 +19,9 @@ export function Profile() {
         </span>
       </div>
 
-      <button
-        type="button"
-        className="ml-auto rounded-md p-2 transition-colors hover:bg-zinc-50"
-      >
+      <Button type="button" variant="ghost">
         <LogOut className="h-5 w-5 text-zinc-500" />
-      </button>
+      </Button>
     </div>
   )
 }
