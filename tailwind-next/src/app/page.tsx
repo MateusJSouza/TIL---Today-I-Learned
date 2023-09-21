c42205f06b9d2c519ee2a98d7e9ccb2498e3a6e4
MateusJSouza <mateusjdev@gmail.com>
2023-09-21 15:45:02 -0300

    feat: :lipstick: add button variant

--- tailwind-next/src/app/page.tsx
+++ tailwind-next/src/app/page.tsx
@@ -7,6 +7,7 @@ import { Bold, Italic, Link, List, ListOrdered, Mail } from 'lucide-react'
 import { SelectItem } from './view/components/Form/Select/SelectItem'
 import { Textarea } from './view/components/Form/Textarea'
 import { Label } from './view/components/Form/Label'
+import { Button } from './view/components/Button'
 
 export default function Home() {
   return (
@@ -24,19 +25,12 @@ export default function Home() {
             </span>
           </section>
           <section className="flex items-center gap-2">
-            <button
-              type="button"
-              className="rounded-lg border border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50"
-            >
+            <Button type="button" variant="outline">
               Cancel
-            </button>
-            <button
-              type="submit"
-              form="settings"
-              className="rounded-lg bg-violet-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-700"
-            >
+            </Button>
+            <Button variant="primary" type="submit" form="settings">
               Save
-            </button>
+            </Button>
           </section>
         </div>
         <form
@@ -194,18 +188,12 @@ export default function Home() {
           </div>
 
           <div className="flex items-center justify-end gap-2 pt-5">
-            <button
-              type="button"
-              className="rounded-lg border border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50"
-            >
+            <Button type="button" variant="outline">
               Cancel
-            </button>
-            <button
-              type="submit"
-              className="rounded-lg bg-violet-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-700"
-            >
+            </Button>
+            <Button variant="primary" type="submit" form="settings">
               Save
-            </button>
+            </Button>
           </div>
         </form>
       </div>
