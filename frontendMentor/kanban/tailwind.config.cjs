157330570872690894ca6eec95b4faa061af1bd9
MateusJSouza <mateusjdev@gmail.com>
2024-09-16 20:51:08 -0300

    config: customizing Tailwind theme with project-specific colors

--- frontendMentor/kanban/tailwind.config.cjs
+++ frontendMentor/kanban/tailwind.config.cjs
@@ -1,26 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        black: '#000112',
-        veryDark: '#20212C',
-        darkGrey: '#2B2C37',
-        linesDark: '#3E3F4E',
-        mediumGrey: '#828FA3',
-        linesLight: '#E4EBFA',
-        lightGrey: '#F4F7FD',
-        white: '#FFFFFF',
-        red: '#EA5555',
-        redHover: '#FF9898',
-        mainPurple: '#635FC7',
-        mainPurpleHover: '#A8A4FF'
-      }
-    },
-  },
-  plugins: [],
-}
+	darkMode: "class",
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				"main-purple": "#635FC7",
+				"main-purple-hover": "#A8A4FF",
+				black: "#000112",
+				"very-dark-grey": "#20212C",
+				"dark-grey": "#2B2C37",
+				"lines-dark": "#3E3F4E",
+				"medium-grey": "#828FA3",
+				"lines-light": "#E4EBFA",
+				"light-grey": "#F4F7FD",
+				white: "#FFFFFF",
+				"main-red": "#EA5555",
+				"main-red-hover": "#FF9898",
+			},
+		},
+	},
+	plugins: [],
+};
