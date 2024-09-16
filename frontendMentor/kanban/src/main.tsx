cb8fce9ab7eb4eff4cc5009277f4935f264b0c34
MateusJSouza <mateusjdev@gmail.com>
2024-09-16 20:50:51 -0300

    feat: setting up main React application with ThemeProvider

--- frontendMentor/kanban/src/main.tsx
+++ frontendMentor/kanban/src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { App } from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./app";
+import "./index.css";
+import { ThemeProvider } from "./contexts/theme";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+	<React.StrictMode>
+		<ThemeProvider>
+			<App />
+		</ThemeProvider>
+	</React.StrictMode>,
+);
