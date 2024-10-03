e644b0ca2927f5a69be7574f7300586d8242be87
MateusJSouza <mateusjdev@gmail.com>
2024-10-03 14:07:26 -0300

    feat: reverse array

---
+++ hackerrank/python/reverse-array.py
@@ -0,0 +1,9 @@
+#
+# Complete the 'reverseArray' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def reverseArray(a):
+    return a[::-1]
